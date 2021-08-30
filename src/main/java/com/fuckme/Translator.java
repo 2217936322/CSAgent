f64a150c6391b4eeafac7e18b9c44548c91245f2
Twi1ight <Twi1ight@T00ls.Net>
2021-08-30 13:40:33 +0800

    maven

--- src/main/java/com/fuckme/Translator.java
+++ src/main/java/com/fuckme/Translator.java
@@ -1,5 +1,8 @@
 package com.fuckme;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
